50eba585501ec5918ac97e4f0cc60af87b6628ec
Iya Vaksina <iya-iysha@mail.ru>
2023-02-16 02:09:04 +0300

    Исправление после ревью_1

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,13 @@ let profile = document.querySelector('.profile');
 let editBtn = profile.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closeBtn = popup.querySelector('.popup__close-btn');
-let nameInput = popup.querySelector('#name');
-let jobInput = popup.querySelector('#job');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_job');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubtitle = profile.querySelector('.profile__subtitle');
 let popupForm = popup.querySelector('.popup__form');
 
-function editProfile() {
+function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
@@ -22,9 +22,9 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
-editBtn.addEventListener('click', editProfile);
+editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', handleFormSubmit);
